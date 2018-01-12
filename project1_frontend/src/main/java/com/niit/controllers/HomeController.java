dc3bfac620669713b2209b43b5a0ff87981fe188
Ibrahimjamaludeen <35214428+Ibrahimjamaludeen@users.noreply.github.com>
2018-01-12 13:43:24 +0530

    Add files via upload

--- project1_frontend/src/main/java/com/niit/controllers/HomeController.java
+++ project1_frontend/src/main/java/com/niit/controllers/HomeController.java
@@ -1,9 +1,14 @@
 package com.niit.controllers;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.niit.services.ProductService;
 
 @Controller
 public class HomeController {
@@ -11,11 +16,23 @@ public class HomeController {
 		System.out.println("HomeController is instantiated");
 	}
 	@RequestMapping(value="/home")
-public String homePage(){
-	return "home";//logical view name
-}
+	public String homePage(HttpSession session, ProductService productService){
+			session.setAttribute("categories", productService.getAllCategories());
+		return "home";//logical view name
+	}
 	@RequestMapping("/aboutus")
 public String aboutUs(){
 	return "aboutUs";
 }
+	
+	//for logout -> /login?logout=''
+	//for invalid credentials -> /login?error=''
+	@RequestMapping("/login")
+	public String loginPage(@RequestParam(required=false) String error,@RequestParam(required=false) String logout,Model model){
+		if(error!=null)
+		model.addAttribute("error","Invalid Username/Password");
+		if(logout!=null)
+			model.addAttribute("msg","Loggedout successfully");
+		return "login";
+	}
 }
